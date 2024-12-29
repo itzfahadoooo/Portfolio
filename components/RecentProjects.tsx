a0aac36ae729fd0308dc577e46c61d9f70b2420c
itzfahadoooo <foyindolapo@gmail.com>
2024-12-29 09:56:25 +0100

    Daily commit

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,6 +1,7 @@
 import { projects } from "@/data";
 import { div } from "framer-motion/client";
 import React from "react";
+import { PinContainer } from "./ui/3d-pin";
 
 const RecentProjects = () => {
   return (
@@ -11,7 +12,19 @@ const RecentProjects = () => {
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
-          <div key={id} className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw] ">{title}</div>
+          <div key={id} className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw] ">
+            <PinContainer title={title} href={link} >
+                <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+                    <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
+                        <img src="/bg.png" alt="bg-img" />
+                    </div>
+                    <img src={img} alt={title} className="z-10 absolute bottom-0" />
+                </div>
+                <h1>
+                    {title}
+                </h1>
+            </PinContainer>
+          </div>
         ))}
       </div>
     </div>
