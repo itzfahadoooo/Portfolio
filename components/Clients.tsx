cfb73bda7071e5594ccf0b85f00b0bb46cc62287
itzfahadoooo <foyindolapo@gmail.com>
2024-12-31 22:56:38 +0100

    Daily commit

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -11,7 +11,8 @@ const Clients = () => {
       <h1 className="heading">
         Kind words from
         <span className="text-purple"> satisfied clients</span>
-      </h1><p></p>
+      </h1>
+      
 
       <div className="flex flex-col items-center max-lg:mt-10">
         <InfiniteMovingCards
