9dfe632061257725fcb9b83ef4ba985ed4af7eda
itzfahadoooo <foyindolapo@gmail.com>
2025-01-01 22:05:13 +0100

    Daily commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import MagicButton from "./ui/MagicButton";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
+    <footer className="w-full pb-10 mb-[100px]" id="contact">
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
