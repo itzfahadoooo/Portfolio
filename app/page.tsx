9389ee4c72ff5188de91506856e0314259b30569
itzfahadoooo <foyindolapo@gmail.com>
2025-01-01 22:01:34 +0100

    Daily commit

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,11 @@ import RecentProjects from "@/components/RecentProjects";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 
 const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
@@ -21,6 +22,7 @@ const Home = () => {
         <Clients/>
         <Experience/>
         <Approach/>
+        <Footer/>
       </div>
     </main>
   );
