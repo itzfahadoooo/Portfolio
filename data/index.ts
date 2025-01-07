e02d1fcc0eca202ffc0182cba466b1009cb4b70f
itzfahadoooo <foyindolapo@gmail.com>
2025-01-07 22:16:18 +0100

    Daily commit

--- data/index.ts
+++ data/index.ts
@@ -173,29 +173,29 @@ export const navItems = [
   export const workExperience = [
     {
       id: 1,
-      title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      title: "Frontend Developer Intern",
+      desc: "Contributed to the development of user-friendly websites and platforms using React.js, improving functionality and performance.",
       className: "md:col-span-2",
       thumbnail: "/exp1.svg",
     },
     {
       id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      title: "MERN Stack Developer - Triune BuiltTech Solutions",
+      desc: "Built full-stack applications, including a food delivery website with React.js, MongoDB, Express, Node.js, and Stripe integration.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp2.svg",
     },
     {
       id: 3,
       title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      desc: "Designed and developed custom websites for clients, translating unique requirements into visually engaging and responsive platforms.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp3.svg",
     },
     {
       id: 4,
       title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      desc: "Crafted and optimized modern user interfaces with React.js, Tailwind CSS, and Bootstrap, ensuring seamless user experiences.",
       className: "md:col-span-2",
       thumbnail: "/exp4.svg",
     },
