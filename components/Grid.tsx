64bd2b0ad7f73ef3ec2ff582d7c4a4940c14e387
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-08-25 00:11:27 +0100

    Initial commit

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -5,12 +5,7 @@ import Photo from "./Photo";
 const Grid = () => {
   return (
     <section id="about">
-      <div className="hidden lg:flex items-center justify-center lg:gap-24 xl:gap-32 2xl:gap-56 2xl:mt-52">
-        <h1 className="heading">
-          About <span className="dark:text-purple text-white-100">moi.....</span>
-        </h1>
-        <Photo />
-      </div>
+      <Photo />
 
       <BentoGrid className="w-full pt-20">
         {gridItems.map(
--- components/Photo.tsx
+++ components/Photo.tsx
@@ -4,70 +4,76 @@ import Image from "next/image";
 
 const Photo = () => {
   return (
-    <div className="cursor-pointer relative flex items-center justify-center">
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          transition: { delay: 2, duration: 0.4, ease: "easeIn" },
-        }}
-      >
-        {/* Image */}
+    <div className="cursor-pointer flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-24 xl:gap-32 2xl:gap-56 2xl:mt-52 relative z-10">
+      <h1 className="heading">
+        About <span className="dark:text-purple text-white-100">moi.....</span>
+      </h1>
+      
+      <div className="relative flex items-center justify-center">
         <motion.div
-          initial={{ opacity: 0, y: 50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true, amount: 0.3 }}
-          transition={{ duration: 0.5, ease: "easeOut" }}
-          className="absolute inset-0 flex items-center justify-center"
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2, duration: 0.4, ease: "easeIn" },
+          }}
         >
-          <Image
-            src="/Headshot5.png"
-            alt="Fahad's Photo"
-            width={298}
-            height={298}
-            priority
-            quality={100}
-            className="object-contain rounded-full opacity-60 hover:opacity-80 transition-opacity duration-500"
-          />
-        </motion.div>
-
-        {/* Circle */}
-        <motion.svg
-          className="w-[307px] h-[307px] text-white-100 dark:text-purple"
-          fill="transparent"
-          viewBox="0 0 506 506"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <motion.circle
-            cx="253"
-            cy="253"
-            r="250"
-            stroke="currentColor"
-            strokeWidth="8"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            initial={{
-              opacity: 0,
-              strokeDasharray: "24 10 0 0",
-            }}
-            whileInView={{
-              opacity: 1,
-              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
-              rotate: [120, 360],
-            }}
+          {/* Image */}
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true, amount: 0.3 }}
-            transition={{
-              opacity: { duration: 0.5, ease: "easeOut" },
-              strokeDasharray: {
-                duration: 20,
-                repeat: Infinity,
-                repeatType: "reverse",
-              },
-              rotate: { duration: 20, repeat: Infinity, repeatType: "reverse" },
-            }}
-          />
-        </motion.svg>
-      </motion.div>
+            transition={{ duration: 0.5, ease: "easeOut" }}
+            className="absolute inset-0 flex items-center justify-center"
+          >
+            <Image
+              src="/Headshot5.png"
+              alt="Fahad's Photo"
+              width={298}
+              height={298}
+              priority
+              quality={100}
+              className="object-contain rounded-full opacity-60 hover:opacity-80 transition-opacity duration-500"
+            />
+          </motion.div>
+
+          {/* Circle */}
+          <motion.svg
+            className="w-[307px] h-[307px] text-white-100 dark:text-purple"
+            fill="transparent"
+            viewBox="0 0 506 506"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <motion.circle
+              cx="253"
+              cy="253"
+              r="250"
+              stroke="currentColor"
+              strokeWidth="8"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              initial={{
+                opacity: 0,
+                strokeDasharray: "24 10 0 0",
+              }}
+              whileInView={{
+                opacity: 1,
+                strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+                rotate: [120, 360],
+              }}
+              viewport={{ once: true, amount: 0.3 }}
+              transition={{
+                opacity: { duration: 0.5, ease: "easeOut" },
+                strokeDasharray: {
+                  duration: 20,
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                },
+                rotate: { duration: 20, repeat: Infinity, repeatType: "reverse" },
+              }}
+            />
+          </motion.svg>
+        </motion.div>
+      </div>
     </div>
   );
 };
